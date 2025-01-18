d3d6667327e2a784e1ce0cf13a44b4ef10ebb546
Arktomson <2372929539@qq.com>
2025-01-18 14:39:14 +0800

    feat: hooks导入变动

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,11 @@
-export * from './useBoolean';
-export * from './useClickOutside';
-export * from './useCreation';
-export * from './useCssVar';
-export * from './useIntersection';
-export * from './useLatest';
-export * from './useLockFn';
-export * from './useMap';
-export * from './useMount';
-export * from './useReactive';
-export * from './useResize';
-export * from './useSafeState';
-export * from './useThrottle';
-export * from './useUnMount';
-export * from './useUnMountedRef';
-export * from './useUpdate';
+const context = require.context('./', true, /index\.tsx?$/);
+
+context.keys().forEach((key) => {
+  if (key !== './index.ts') {
+    Object.keys(context(key)).forEach((exportName) => {
+      if (exportName !== 'default') {
+        exports[exportName] = context(key)[exportName];
+      }
+    });
+  }
+});
